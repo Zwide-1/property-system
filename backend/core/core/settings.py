7c22ee130f61cddd8dde3a6141b5714e4b1047c6
Zwide1 <zwide3bn@gmail.com>
2026-06-18 16:03:59 +0200

    Fix static files

--- backend/core/core/settings.py
+++ backend/core/core/settings.py
@@ -50,8 +50,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_celery_beat',
-    'cloudinary',
-    'cloudinary_storage',
     
 
     'rest_framework',
@@ -68,6 +66,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'core.urls'
@@ -158,6 +157,10 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+STATICFILES_STORAGE = (
+    "whitenoise.storage.CompressedManifestStaticFilesStorage"
+)
 
 
 CORS_ALLOWED_ORIGINS = [
